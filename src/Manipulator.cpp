4786dac473e6ffc50592883865a4e7050724df4b
sam <samlovelace12@gmail.com>
2025-04-22 20:05:45 -0400

    updates to just use kdl to construct the tree from urdf

--- src/Manipulator.cpp
+++ src/Manipulator.cpp
@@ -1,7 +1,6 @@
 
 #include "Manipulator.h"
-//#include <kdl_parser/kdl_parser/kdl_parser.hpp>
-#include <urdf/model.h> 
+#include <kdl_parser/kdl_parser.hpp>
 #include "ManipulatorFactory.h"
 #include "plog/Log.h"
 
@@ -15,15 +14,14 @@ Manipulator::Manipulator(ConfigManager::Config aConfig) : mConfig(aConfig)
     std::string urdfFilePath = mConfig.shareDir + mConfig.manipType + "/manipulator.urdf";
     LOGW << "urdfFilePath: " << urdfFilePath;
 
-    urdf::Model model;
-    if(!model.initFile(urdfFilePath))
+    KDL::Tree tree; 
+    if(!kdl_parser::treeFromFile(urdfFilePath, tree))
     {
-        LOGE << "Could not load manipulator urdf at " << urdfFilePath; 
+        LOGE << "Failed to parse urdf to KDL::Tree"; 
         return; 
-    }
+    } 
 
-    LOGD << "Parsed urdf for " << model.getName(); 
-
+    LOGD << "Parsed manipulator with " << tree.getNrOfJoints() << " joints"; 
 }
 
 Manipulator::~Manipulator()
