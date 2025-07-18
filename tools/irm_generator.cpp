d563ccf3f8bb20f529986128b0cede82e77e1e1e
samlovelace <samlovelace12@gmail.com>
2025-07-18 19:03:34 -0400

    updates irm_generator to compile against configmanager and kinematicshandler

--- tools/irm_generator.cpp
+++ tools/irm_generator.cpp
@@ -1,7 +1,27 @@
-#include <cstdio> 
+#include <iostream>
+#include "ConfigManager.h"
+#include <ament_index_cpp/get_package_share_directory.hpp>
+#include "KinematicsHandler.h"
 
 int main()
 {
-    printf("Hello, irm\n"); 
+    std::string package_path = ament_index_cpp::get_package_share_directory("arm_configs");
+    std::string config_path = package_path + "/config.yaml";
+    auto configManager = ConfigManager::getInstance(); 
+    configManager->loadConfig(config_path);
+
+    auto config = configManager->getConfig(); 
+
+    std::string urdfFilePath = config.shareDir + "manipulators/" + config.manipType + "/manipulator.urdf";
+    std::cout << "urdfFilePath: " << urdfFilePath << std::endl; 
+
+    KinematicsHandler kh; 
+    if(!kh.init(urdfFilePath))
+    {
+        std::cerr << "Failed to initialize kinematics handler\n"; 
+        return 0; 
+    } 
+
+    std::cout << "Initializd kinematics handler!\n"; 
     return 0; 
 }
