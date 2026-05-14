b12096808f3f80cfbdcfb523b1b1ea8f86630542
samlovelace <samlovelace12@gmail.com>
2026-05-13 22:01:47 -0400

    adds arm_msgs pkg, updates arm_hardware to publish hardware state

--- arm/arm_hardware/src/HardwareManager.cpp
+++ arm/arm_hardware/src/HardwareManager.cpp
@@ -1,7 +1,12 @@
 
 #include <ament_index_cpp/get_package_share_directory.hpp>
+#include <arm_common/RosTopicManager.hpp>
+#include <arm_msgs/msg/detail/hardware_state__struct.hpp>
+#include <kdl/jntarray.hpp>
 #include <string>
 #include <plog/Log.h>
+
+#include "arm_msgs/msg/hardware_state.hpp"
 
 #include "arm_hardware/HardwareManager.h"
 #include "arm_hardware/ManipulatorFactory.h"
@@ -22,6 +27,8 @@ HardwareManager::~HardwareManager()
 
 bool HardwareManager::init()
 {
+    RosTopicManager::getInstance()->createPublisher<arm_msgs::msg::HardwareState>("arm/hardware_state");
+
     mComms = ManipulatorFactory::create(ConfigManager::getInstance()->getManipConfig());
     mComms->init();
 
@@ -39,9 +46,29 @@ void HardwareManager::run()
     RateController rate(20); // TODO: make config
     mRunning = true;
 
+    KDL::JntArray pos, vel;
+    std::vector<double> posV, velV;
+
     while(mRunning)
     {
         rate.start();
+
+        pos = mComms->getJointPositions();
+        vel = mComms->getJointPositions();
+        posV.clear(); velV.clear();
+        posV.resize(pos.rows()); velV.resize(vel.rows());
+
+        for(int i = 0; i < (int)pos.rows(); i++)
+        {
+            posV[i] = pos(i);
+            velV[i] = vel(i);
+        }
+
+        arm_msgs::msg::HardwareState state;
+        state.set__position(posV);
+        state.set__velocity(velV);
+
+        RosTopicManager::getInstance()->publishMessage("arm/hardware_state", state);
 
         rate.block();
     }
